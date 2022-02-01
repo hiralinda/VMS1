99664183d4bf4ebe9634e08def8f42fac9210d64
Angel Santana <n01257615@unf.edu>
2022-02-01 04:05:29 -0500

    profile pic added

--- VMS/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ VMS/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -110,6 +112,17 @@ namespace VMS.Areas.Identity.Pages.Account.Manage
             var address = user.address;
             var zip = user.zip;
 
+            if (Request.Form.Files.Count > 0)
+            {
+                IFormFile file = Request.Form.Files.FirstOrDefault();
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    user.ProfilePicture = dataStream.ToArray();
+                }
+                await _userManager.UpdateAsync(user);
+            }
+
             if (Input.FirstName != firstName)
             {
                 user.FirstName = Input.FirstName;
@@ -119,12 +132,6 @@ namespace VMS.Areas.Identity.Pages.Account.Manage
             if (Input.LastName != lastName)
             {
                 user.LastName = Input.LastName;
-                await _userManager.UpdateAsync(user);
-            }
-
-            if (Input.ProfilePicture != profilePicture)
-            {
-                user.ProfilePicture = Input.ProfilePicture;
                 await _userManager.UpdateAsync(user);
             }
 
