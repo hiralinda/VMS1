99e14f218e20d0b130fb4da7fe4f898f5affa863
Angel Santana <n01257615@unf.edu>
2022-02-04 03:36:22 -0500

    fixed login bug and added organization name to profile

--- VMS/Startup.cs
+++ VMS/Startup.cs
@@ -32,7 +32,7 @@ namespace VMS
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders()
