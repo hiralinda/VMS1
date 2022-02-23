710c56f99e1c7fb5bddb53306202dd273dd69a0b
Angel Santana <n01257615@unf.edu>
2022-02-23 15:06:43 -0500

    created a basic application model for volunteer signup

---
+++ VMS/Models/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VMS.Models
+{
+    public class Application
+    {
+        public int id { get; set; }
+        public ApplicationUser nonprofit { get; set; }
+        public ApplicationUser volunteer { get; set; }
+        public string questionResponse { get; set; }
+        public bool status { get; set; }
+    }
+}
