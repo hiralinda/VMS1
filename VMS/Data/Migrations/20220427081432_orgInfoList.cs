3a76eaed0e00caa001a4b1a5a7b1029d1b4d1263
Angel Santana <n01257615@unf.edu>
2022-04-27 05:17:09 -0400

    Home page changes

---
+++ VMS/Data/Migrations/20220427081432_orgInfoList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VMS.Data.Migrations
+{
+    public partial class orgInfoList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreateOrgName",
+                table: "Opportunity",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "CreateOrgPic",
+                table: "Opportunity",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateOrgName",
+                table: "Opportunity");
+
+            migrationBuilder.DropColumn(
+                name: "CreateOrgPic",
+                table: "Opportunity");
+        }
+    }
+}
---
+++ VMS/Data/Migrations/20220427082323_removeUnusedFields.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VMS.Data.Migrations
+{
+    public partial class removeUnusedFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateOrgName",
+                table: "Opportunity");
+
+            migrationBuilder.DropColumn(
+                name: "CreateOrgPic",
+                table: "Opportunity");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreateOrgName",
+                table: "Opportunity",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "CreateOrgPic",
+                table: "Opportunity",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
