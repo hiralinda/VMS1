69e717ebc7740a2c7d07ebcd08d05bc687257e4d
Guilherme Santos <gui.srodrigues@hotmail.com>
2023-01-25 22:49:54 -0300

    update culture info

--- VMS/Startup.cs
+++ VMS/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using VMS.Data;
@@ -43,7 +44,11 @@ namespace VMS
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
+			var cultureInfo = new CultureInfo("en-US");
+			CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+			CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+			app.UseDeveloperExceptionPage();
             app.UseMigrationsEndPoint();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
